65b50fe11d6f70e5bbf733f702e6ddc4a48dc22b
Andre van Tonder <drevtonder@gmail.com>
2018-06-17 15:26:43 +0800

    change server script

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,22 +1,3 @@
-import express from "express";
-import http from "http";
-import socketio from "socket.io";
+import Server from "./server";
 
-const app = express();
-const server = new http.Server(app);
-const io = socketio(server);
-
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-io.on("connection", (socket) => {
-  console.log("a user connected");
-  socket.on("disconnect", () => {
-      console.log("user disconnected");
-    });
-});
-
-server.listen(3000, () => {
-  console.log("listening on *:3000");
-});
+Server.start();
