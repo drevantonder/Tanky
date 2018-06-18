c9e457bd28d88dacdc9c103a5182ee495eff0311
Andre van Tonder <drevtonder@gmail.com>
2018-06-18 09:19:47 +0800

    silence shell commands and clear console before run

--- gulpfile.js
+++ gulpfile.js
@@ -3,24 +3,25 @@ const run = require('gulp-run');
 
 gulp.task('build:server:clean', function() {
   console.log('Cleaning build folder...');
-  return run('yarn run clean').exec();
+  return run('yarn run clean', { silent: true }).exec();
 });
 
 gulp.task('build:server:compile', ['build:server:clean'], function() {
   console.log('Compiling Typescript...');
-  return run('yarn run compile').exec();
+  return run('yarn run compile', { silent: true }).exec();
 });
 
 gulp.task('build:server',['build:server:clean', 'build:server:compile']);
 
 gulp.task('build:client:webpack', function() {
   console.log("Webpacking...")
-  return run('npx webpack').exec();
+  return run('npx webpack', { silent: true }).exec();
 });
 
 gulp.task('build:client',['build:client:webpack']);
 
 gulp.task('run:server', ['build:server', 'build:client'], function(){
+  console.clear();
   console.log('Running Server...');
   return run('yarn run server').exec();
 });
