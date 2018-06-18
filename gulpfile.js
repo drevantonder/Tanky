4afd5d154143daa7ceb0fe2e7876f5f11ff8dd0d
Andre van Tonder <drevtonder@gmail.com>
2018-06-18 16:37:52 +0800

    made gulp watch not run server but instead just build

--- gulpfile.js
+++ gulpfile.js
@@ -3,11 +3,11 @@ const run = require('gulp-run');
 
 gulp.task('build:server:clean', () => run('yarn run clean', { silent: true }).exec());
 
-gulp.task('build:server:compile', ['build:server:clean'], () => run('yarn run compile', { silent: true }).exec());
+gulp.task('build:server:compile', ['build:server:clean'], () => run('yarn run compile').exec());
 
 gulp.task('build:server',['build:server:clean', 'build:server:compile']);
 
-gulp.task('build:client:webpack', () => run('npx webpack', { silent: true }).exec());
+gulp.task('build:client:webpack', () => run('npx webpack').exec());
 
 gulp.task('build:client',['build:client:webpack']);
 
@@ -20,6 +20,6 @@ gulp.task('run:server', ['build:server', 'build:client'], () => {
 gulp.task('default', ['build:client', 'build:server', 'run:server'], () => console.log('Done'));
 
 gulp.task('watch', () => {
-  gulp.watch("src/server/**/*", ['run:server'])
-  gulp.watch("src/client/**/*", ['run:server'])
+  gulp.watch("src/server/**/*", ['build:server'])
+  gulp.watch("src/client/**/*", ['build:client'])
 })
