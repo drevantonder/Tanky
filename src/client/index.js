170e612ad2917ccf0c22c3f30daba3b28bb8fa5e
Andre van Tonder <drevtonder@gmail.com>
2018-06-20 21:09:49 +0800

    create player from Json

--- src/client/index.js
+++ src/client/index.js
@@ -1,8 +1,10 @@
 import io from 'socket.io-client'
 var socket = io()
 
+import Player from '../imports/player'
+
 socket.on('new-player', (data) => {
-  console.log(data)
+  console.log(new Player(data))
 })
 
 import Game from './game'
