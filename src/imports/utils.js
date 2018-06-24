bfd1cea566c63b774959198cc1de7eb019deea86
Andre van Tonder <drevtonder@gmail.com>
2018-06-25 07:05:32 +0800

    remove uneeded function

--- src/imports/utils.js
+++ src/imports/utils.js
@@ -3,13 +3,5 @@ const uuidV4 = require('uuid/v4')
 module.exports = {
   getUID(){
     return uuidV4()
-  },
-
-  readData(data, key, defaultFunction = () => {}){
-    if(data){
-      return data[key] ? data[key] : defaultFunction()
-    } else {
-      return defaultFunction()
-    }
   }
 }
