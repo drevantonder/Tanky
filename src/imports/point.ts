bebe400550c6d1ce9578b55652a4aeb8678e0d41
Andre van Tonder <drevtonder@gmail.com>
2018-06-28 11:44:50 +0800

    add some basic math operations to point

--- src/imports/point.ts
+++ src/imports/point.ts
@@ -6,6 +6,33 @@ export class Point {
         this.y = y;
     }
 
+    add(n: any) {
+        if (typeof n === "number") {
+            return new Point(this.x + n, this.y + n);
+        } else if (n instanceof Point) {
+            return new Point(this.x + n.x, this.y + n.y);
+        }
+        throw new Error("Method not implemented.");
+    }
+
+    multiply(n: any) {
+        if (typeof n === "number") {
+            return new Point(this.x * n, this.y * n);
+        } else if (n instanceof Point) {
+            return new Point(this.x * n.x, this.y * n.y);
+        }
+        throw new Error("Method not implemented.");
+    }
+
+    subtract(n: any) {
+        if (typeof n === "number") {
+            return new Point(this.x - n, this.y - n);
+        } else if (n instanceof Point) {
+            return new Point(this.x - n.x, this.y - n.y);
+        }
+        throw new Error("Method not implemented.");
+    }
+
     toString() {
         return this.x.toString() + "," + this.y.toString();
     }
